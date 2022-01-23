e49639a715da55b965b625e404a7ea5608fb2b04
theeastwindz0 <developerhimanshuchauhan@gmail.com>
2022-01-23 15:05:22 +0530

    Fixed Parallex Image & Button border

--- src/components/BasicComp/Button.js
+++ src/components/BasicComp/Button.js
@@ -1,14 +1,29 @@
-import { Link } from 'react-router-dom'
-import '../CSS/Button.css'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import "../CSS/Button.css";
+import VarContext from "../store/VarContext";
 const Button = (props) => {
-    
-    return (
-       <>
-       {props.buttons.map((button,key)=>(
-           <Link onClick={props.toggleOff} key={key} className='Button' to={button.location}>{button.title}</Link>
-       ))}
-       </>
-    )
-}
+  const varCtx = useContext(VarContext);
+  let style;
+  if (varCtx.currentMode === "Light Mode")
+    style = { border: "2px solid purple" };
+  else style = { border: '2px solid white' };
 
-export default Button
+  return (
+    <>
+      {props.buttons.map((button, key) => (
+        <Link
+          onClick={props.toggleOff}
+          key={key}
+          style={style}
+          className="Button"
+          to={button.location}
+        >
+          {button.title}
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default Button;
